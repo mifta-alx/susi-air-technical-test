b559eecedc10ca90fd14992d576128ce4d59196b
Miftakhussurur Al Maliki <alx.yoi67@gmail.com>
2026-07-14 19:39:53 +0700

    setup pinia nuxt

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -10,7 +10,7 @@ export default defineNuxtConfig({
       include: ["@lucide/vue"],
     },
   },
-  modules: ["@vite-pwa/nuxt"],
+  modules: ["@vite-pwa/nuxt", "@pinia/nuxt"],
   pwa: {
     strategies : "generateSW",
     registerType: "autoUpdate",
